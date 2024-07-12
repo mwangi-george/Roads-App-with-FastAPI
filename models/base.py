63fd7f068aa404c6a68c2d6238d5599714f3e39a
George Mwangi <mwangigeorge648@gmail.com>
2024-07-12 11:59:16 +0300

    Database session

--- models/base.py
+++ models/base.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 from config import Config
 config = Config()
@@ -8,4 +9,5 @@ DATABASE_URL = f"postgresql://{config.DB_HOST}/{
     config.DB_NAME}?user={config.DB_USERNAME}&password={config.DB_PASSWORD}"
 
 engine = create_engine(url=DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
