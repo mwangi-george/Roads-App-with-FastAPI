8549933d24f1b36521a8137c939c0da19614e892
George Mwangi <mwangigeorge648@gmail.com>
2024-07-12 10:37:12 +0300

    Base file (DB)

--- models/base.py
+++ models/base.py
@@ -1,5 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
+from config import Config
+config = Config()
+
 DATABASE_URL = f"postgresql://{config.DB_HOST}/{
     config.DB_NAME}?user={config.DB_USERNAME}&password={config.DB_PASSWORD}"
+
+engine = create_engine(url=DATABASE_URL)
+Base = declarative_base()
