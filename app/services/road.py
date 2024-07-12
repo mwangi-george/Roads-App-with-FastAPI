60109d91747072d28df3eb7a5e4abdcf1251c5b5
George Mwangi <mwangigeorge648@gmail.com>
2024-07-12 19:42:20 +0300

    Updated crud file for road endpoints

--- app/services/road.py
+++ app/services/road.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
 from models.roads import Roads
-from app.schemas.road import Road
+from app.schemas.road import Road, RoadCreate
+from fastapi import HTTPException, status
 
 
 class RoadServices:
@@ -8,9 +9,33 @@ class RoadServices:
         pass
 
     @staticmethod
-    def get_road_by_id(road_id: int, db: Session):
-        return db.query(Roads).filter(Roads.road_id == road_id).first()
+    def new_road(
+        start_loaction_id: int,
+        end_location_id: int,
+        road_details: RoadCreate,
+        db: Session
+    ) -> str:
+        try:
+            db_road = Roads(
+                name=road_details.name,
+                length_km=road_details.length_km,
+                construction_year=road_details.construction_year,
+                start_location_id=start_loaction_id,
+                end_location_id=end_location_id
+            )
+            db.add(db_road)
+            db.commit()
+            db.refresh(db_road)
+            return "Road createed successfully"
+        except Exception:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="This road's start or end location id is not available on the server. Please ensure the start and end locations are registered or contact API author for clarification"
+            )
 
-    @staticmethod
-    def get_multiple_roads(db: Session, skip: int = 0, limit: int = 100):
-        return db.query(Roads).offset(skip).limit(limit).all()
+    # @staticmethod
+    # def get_road_by_id(road_id: int, db: Session):
+    #     return db.query(Roads).filter(Roads.road_id == road_id).first()
+    # @staticmethod
+    # def get_multiple_roads(db: Session, skip: int = 0, limit: int = 100):
+    #     return db.query(Roads).offset(skip).limit(limit).all()
