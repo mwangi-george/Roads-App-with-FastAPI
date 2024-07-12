51a420536bc06dbd5701fb171d67a1680fa8ffb1
George Mwangi <mwangigeorge648@gmail.com>
2024-07-12 11:55:41 +0300

    Database migrations

--- models/locations.py
+++ models/locations.py
@@ -5,7 +5,8 @@ from models.base import Base
 class Locations(Base):
     __tablename__ = "locations"
 
-    location_id = Column(Integer, primary_key=True, index=True)
+    location_id = Column(Integer, primary_key=True,
+                         index=True, autoincrement=True)
     latitude = Column(Float)
     longitude = Column(Float)
     description = Column(String)
--- models/roads.py
+++ models/roads.py
@@ -5,8 +5,9 @@ from models.base import Base
 class Roads(Base):
     __tablename__ = "roads"
 
-    road_id = Column(Integer, primary_key=True, index=True)
+    road_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String, index=True, nullable=False)
     length_km = Column(Float)
     construction_year = Column(Integer, index=True)
-    start_location_id = Column(String, ForeignKey("locations.location_id"))
+    start_location_id = Column(Integer, ForeignKey(
+        "locations.location_id"), nullable=False)
