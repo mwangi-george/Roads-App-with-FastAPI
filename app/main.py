def714f6d46c089bf97d404eb4160e518949c703
George Mwangi <mwangigeorge648@gmail.com>
2024-07-13 19:02:51 +0300

    Home Template added

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from app.routes.road import create_road_router
-from app.routes.locations import create_location_router
+from app.routes import create_home_router, create_road_router, create_location_router
+from fastapi.staticfiles import StaticFiles
 
 
 def create_app() -> FastAPI:
@@ -10,11 +10,16 @@ def create_app() -> FastAPI:
         description="Provides API endpoint for performing CRUD operations on the Roads Database"
     )
 
+    app.mount(path="/static",
+              app=StaticFiles(directory="static"), name="static")
+
+    home_router = create_home_router()
     road_router = create_road_router()
     loction_router = create_location_router()
 
     app.include_router(road_router)
     app.include_router(loction_router)
+    app.include_router(home_router)
     return app
 
 
